f34c494aac9586c360d87a1df8faad320586a322
klloun <gaih.moa@gmail.com>
2026-02-25 11:17:19 +0300

    Первая практика

---
+++ Lesson1/buttonclicker/src/main/java/ru/mirea/aleksandrovma/buttonclicker/MainActivity.java
@@ -0,0 +1,51 @@
+package ru.mirea.aleksandrovma.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvOut;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+    private CheckBox check;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        tvOut = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        check = findViewById(R.id.check);
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Мой номер по списку № 2");
+                check.setChecked(true);
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+    public void onMyButtonClick(View view)
+    {Toast.makeText(this, "Это не я сделал(", Toast.LENGTH_SHORT).show();
+        check.setChecked(false);
+    }
+}
---
+++ Lesson1/control_lesson1/src/main/java/ru/mirea/aleksandrovma/control_lesson1/MainActivity.java
@@ -0,0 +1,44 @@
+package ru.mirea.aleksandrovma.control_lesson1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView5;
+    private int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        textView5 = findViewById(R.id.textView5);
+        textView5.setText(String.valueOf(count));
+        TextView tv = findViewById(R.id.textView2);
+        tv.setText("New text in MIREA");
+        Button button = findViewById(R.id.button);
+        button.setText("MireaButton");
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setChecked(true);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void onMyButtonClick(View view){
+        count += 1;
+        textView5.setText(String.valueOf(count));
+    }
+}
